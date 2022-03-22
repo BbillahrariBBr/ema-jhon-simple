c97c1a2ef0a6eb30818a8d85900d447e184c9bf3
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-22 13:30:23 +0600

    header image add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/HeaderComponents/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       
     </div>
   );
