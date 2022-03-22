f43348414346229d7d9a7bcdb0c3d605880fecf2
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-22 14:18:23 +0600

    load fake data and show

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/HeaderComponents/Header';
+import Shop from './components/ShopComponents/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
       
     </div>
   );
