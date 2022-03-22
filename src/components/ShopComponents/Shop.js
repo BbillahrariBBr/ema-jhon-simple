40e0762c3ee7cd20ebd0febbeaa0c74303cd3fa8
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-22 21:25:22 +0600

    received dynamic data in component

--- src/components/ShopComponents/Shop.js
+++ src/components/ShopComponents/Shop.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Product from '../ProductComponent/Product';
 import './shop.css';
 const Shop = () => {
-    const first10 = fakeData.slice(0,10);
+    const first10 = fakeData.slice(0,15);
     const [products,setProducts] = useState(first10);
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <ul>
-                    {
-                        products.map(product => <li>{product.name}</li>)
-                    }
-                </ul>
+                
+                {
+                    products.map(product => <Product product = {product}></Product>)
+                }
+                
 
             </div>
             <div className="cart-container">
