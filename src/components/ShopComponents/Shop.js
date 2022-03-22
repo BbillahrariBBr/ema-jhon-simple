78f9bc3e306c84eb0e9114d1932409d5a34034ec
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-22 22:26:36 +0600

    almost product all element data load

--- src/components/ShopComponents/Shop.js
+++ src/components/ShopComponents/Shop.js
@@ -3,24 +3,24 @@ import fakeData from '../../fakeData';
 import Product from '../ProductComponent/Product';
 import './shop.css';
 const Shop = () => {
-    const first10 = fakeData.slice(0,15);
-    const [products,setProducts] = useState(first10);
+    const first10 = fakeData.slice(0, 15);
+    const [products, setProducts] = useState(first10);
     return (
         <div className='shop-container'>
             <div className="product-container">
-                
+
                 {
-                    products.map(product => <Product product = {product}></Product>)
+                    products.map(product => <Product product={product}></Product>)
                 }
-                
+
 
             </div>
             <div className="cart-container">
                 <h3>this is cart{products.length}</h3>
 
             </div>
-            
-            
+
+
         </div>
     );
 };
