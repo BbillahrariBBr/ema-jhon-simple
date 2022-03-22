5ee71f877a3a85828c354bb01dbf68adbcb11773
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-22 21:07:53 +0600

    devided two part

--- src/components/ShopComponents/Shop.js
+++ src/components/ShopComponents/Shop.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import './shop.css';
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products,setProducts] = useState(first10);
     return (
-        <div>
-            <h1>This is shop</h1>
-            <h3>{products.length}</h3>
-            <ul>
-                {
-                    products.map(product => <li>{product.name}</li>)
-                }
-            </ul>
+        <div className='shop-container'>
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map(product => <li>{product.name}</li>)
+                    }
+                </ul>
+
+            </div>
+            <div className="cart-container">
+                <h3>this is cart{products.length}</h3>
+
+            </div>
+            
+            
         </div>
     );
 };
